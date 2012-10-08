539d9d534d00c89bacfa5a7fa25079b8e0367baa
prudhviy <prudhviy@gmail.com>
2012-10-08 20:53:38 +0530

    fmt fix

--- chat.go
+++ chat.go
@@ -1,27 +1,27 @@
 package main
 
 import (
-    "time"
-    
-    "io"
-    "io/ioutil"
-    //"bytes"
-    "net/http"
-    "os"
-    
-    //"crypto/md5"
-    //"hash"
+	"time"
 
-    //"html/template"
-    "log"
-    "fmt"
-    //"strings"
-    //"encoding/json"
-    //"encoding/hex"
+	"io"
+	"io/ioutil"
+	//"bytes"
+	"net/http"
+	"os"
 
-    "runtime"
-    "flag"
-    //"reflect"
+	//"crypto/md5"
+	//"hash"
+
+	//"html/template"
+	"log"
+	"fmt"
+	//"strings"
+	//"encoding/json"
+	//"encoding/hex"
+
+	"runtime"
+	"flag"
+	//"reflect"
 )
 
 var numCores = flag.Int("n", runtime.NumCPU(), "number of CPU cores to use")
@@ -41,14 +41,16 @@ var testPageHTML = `<!DOCTYPE html>
                 data: {'comm_id': comm_id, 'join_time': join_time},
                 url: '/chat/join/',
                 success: function(res){
-                    var ele = '<li>' + res + '</li>'
+                    console.log(typeof res);
+                    console.log(res);
+                    var ele = '<li>' + res.toString() + '</li>'
                     $('.log').append($(ele));
                 },
                 complete: function(){
                     console.log('close conn', join_time);
                     //pp.chat.join(comm_id);
                 },
-                timeout: 60000
+                timeout: 15000
             });
         };
         pp.chat.send_msg = function(comm_id, msg) {
@@ -92,9 +94,9 @@ var testPageHTML = `<!DOCTYPE html>
 `
 
 type commEntity struct {
-    id string
-    recv chan string
-    //status string
+	id   string
+	recv chan string
+	//status string
 }
 
 //var room map[string]Chann = make(map[string]Chann)
@@ -106,110 +108,125 @@ var users map[string]commEntity = make(map[string]commEntity)
 }*/
 
 func testPage(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Cache-Control", "no-cache")
-    w.Header().Set("Content-Type", "text/html; charset=iso-8859-1")
-        
-    io.WriteString(w, testPageHTML)
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-Type", "text/html; charset=iso-8859-1")
+
+	io.WriteString(w, testPageHTML)
 }
 
 func jquery(w http.ResponseWriter, req *http.Request) {
-    w.Header().Set("Cache-Control", "no-cache")
-    w.Header().Set("Content-Type", "application/javascript; charset=iso-8859-1")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-Type",
+		"application/javascript; charset=iso-8859-1")
 
-    resourceData, err := ioutil.ReadFile("./jquery.js")
-    if err != nil {
-       fmt.Printf("###\nError - %s\n###\n", err)
-       os.Exit(1)
-    }
+	resourceData, err := ioutil.ReadFile("./jquery.js")
+	if err != nil {
+		fmt.Printf("###\nError - %s\n###\n", err)
+		os.Exit(1)
+	}
 
-    io.WriteString(w, string(resourceData))
+	io.WriteString(w, string(resourceData))
 }
 
 func sendMessage(w http.ResponseWriter, req *http.Request) {
-    //var err error
-    
-    w.Header().Set("Cache-Control", "no-cache")
-    w.Header().Set("Content-Type", "application/javascript")
-    
-    response := "message sent"
+	//var err error
 
-    recepientUserId := req.FormValue("comm_id")
-    chatMessage := req.FormValue("msg")
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-Type", "application/javascript")
 
-    cEntity := users[recepientUserId]
-    cEntity.recv <- chatMessage
-    
-    io.WriteString(w, response)
-    /*if err == nil {
-        fmt.Printf("Response sent- %s %s\n", req.URL, time.Now())
-    }*/
+	response := "message sent"
+
+	recepientUserId := req.FormValue("comm_id")
+	chatMessage := req.FormValue("msg")
+
+	cEntity := users[recepientUserId]
+	cEntity.recv <- chatMessage
+
+	io.WriteString(w, response)
+	/*if err == nil {
+	    fmt.Printf("Response sent- %s %s\n", req.URL, time.Now())
+	}*/
 }
 
-func openPushChannel(comm_id string, join_time string) (chan string) {
-    var newUser commEntity
-    var userRecvChannel chan string
+func openPushChannel(comm_id string, join_time string) chan string {
+	var newUser commEntity
+	var userRecvChannel chan string
 
-    _, exists := users[comm_id]
-    if exists {
-        fmt.Printf("Already joined> User-id:%v %v\n", comm_id, join_time)
-        userRecvChannel = users[comm_id].recv
-    } else {
-        fmt.Printf("Join Chat> User-id:%v %v\n", comm_id, join_time)
-        newUser.id = comm_id
-        newUser.recv = make(chan string)
-        users[newUser.id] = newUser
-        userRecvChannel = newUser.recv
-    }
-    return userRecvChannel
+	_, exists := users[comm_id]
+	if exists {
+		fmt.Printf("Already joined> User-id:%v %v\n", comm_id, join_time)
+		userRecvChannel = users[comm_id].recv
+	} else {
+		fmt.Printf("Join Chat> User-id:%v %v\n", comm_id, join_time)
+		newUser.id = comm_id
+		newUser.recv = make(chan string)
+		users[newUser.id] = newUser
+		userRecvChannel = newUser.recv
+	}
+	return userRecvChannel
 }
 
 func getChatMessage(recv chan string) (msg string) {
-    timeout := time.After(5 * time.Second)
-    select {
-        case newMessage := <-recv:
-            msg = newMessage
-        case <-timeout:
-            fmt.Printf("timeout yes\n")
-            msg = "timeout"
-    }
-    return msg
+	timeout := time.After(10 * time.Second)
+	select {
+	case newMessage := <-recv:
+		msg = newMessage
+	case <-timeout:
+		fmt.Printf("timeout yes\n")
+		msg = "timeout"
+	}
+	return msg
 }
 
 func joinChat(w http.ResponseWriter, req *http.Request) {
-    
-    w.Header().Set("Cache-Control", "no-cache")
-    w.Header().Set("Content-Type", "text/html")
 
-    fmt.Printf("JoinChat method> User-id:%s\n", req.FormValue("comm_id"))
-    recv := openPushChannel(req.FormValue("comm_id"), req.FormValue("join_time"))
-    newMessage := getChatMessage(recv)
+	w.Header().Set("Cache-Control", "no-cache")
+	w.Header().Set("Content-Type", "text/html")
 
-    if newMessage == "timeout" {
-        conn, _, _ := w.(http.Hijacker).Hijack()
-        fmt.Printf("Timeout- close conn> User-id:%s %s %T\n", req.FormValue("comm_id"), req.FormValue("join_time"), conn)
-        conn.Close()
-        //response.Body.Close?
-    } else {
-        io.WriteString(w, newMessage)
-        fmt.Printf("Chat sent> User-id:%s %s %s\n", req.FormValue("comm_id"), req.FormValue("join_time"), newMessage)
-    }
+	fmt.Printf("JoinChat method> User-id:%s\n", req.FormValue("comm_id"))
+	recv := openPushChannel(req.FormValue("comm_id"),
+		req.FormValue("join_time"))
+	newMessage := getChatMessage(recv)
+
+	if newMessage == "timeout" {
+		hj, ok := w.(http.Hijacker)
+		if !ok {
+			http.Error(w, "webserver doesn't support hijacking",
+				http.StatusInternalServerError)
+		}
+		hjConn, bufrw, err := hj.Hijack()
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		defer hjConn.Close()
+		bs, _ := bufrw.Reader.Peek(1)
+		if len(bs) == 0 {
+			fmt.Printf("client conn closed\n")
+		}
+		bufrw.Flush()
+		fmt.Printf("Timeout- close conn> User-id:%s %s %T\n",
+			req.FormValue("comm_id"),
+			req.FormValue("join_time"), hjConn)
+	} else {
+		io.WriteString(w, newMessage)
+		fmt.Printf("Chat sent> User-id:%s %s %s\n", req.FormValue("comm_id"),
+			req.FormValue("join_time"), newMessage)
+	}
 }
 
 func main() {
-    // make sure app uses all cores
-    flag.Parse()
-    runtime.GOMAXPROCS(*numCores)
+	// make sure app uses all cores
+	flag.Parse()
+	runtime.GOMAXPROCS(*numCores)
+	fmt.Printf("\nChat server running at "+
+		"http://0.0.0.0:8000 on %d CPU cores\n", *numCores)
+	http.HandleFunc("/", testPage)
+	http.HandleFunc("/jquery.js", jquery)
+	http.HandleFunc("/chat/join/", joinChat)
+	http.HandleFunc("/chat/message/", sendMessage)
 
-    fmt.Printf("\nChat server running at " +
-               "http://0.0.0.0:8000 on %d CPU cores\n", *numCores)
-
-    http.HandleFunc("/", testPage)
-    http.HandleFunc("/jquery.js", jquery)
-    http.HandleFunc("/chat/join/", joinChat)
-    http.HandleFunc("/chat/message/", sendMessage)
-
-    err := http.ListenAndServe("0.0.0.0:8000", nil)
-    if err != nil {
-        log.Fatal("In main(): ", err)
-    }
+	err := http.ListenAndServe("0.0.0.0:8000", nil)
+	if err != nil {
+		log.Fatal("In main(): ", err)
+	}
 }
